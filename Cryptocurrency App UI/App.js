f9b854aaa67ffb1185598d0fd6a30c08414363f2
hussain <hussainabdullatif@marun.edu.tr>
2023-11-05 23:39:44 +0300

    Added custom fonts loading checker

--- Cryptocurrency App UI/App.js
+++ Cryptocurrency App UI/App.js
@@ -2,12 +2,23 @@ import React from "react";
 import { CryptoDetail, Transaction } from "./screens";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
+import { useFonts } from "expo-font";
 
 import Tabs from "./navigation/tabs";
 
 const Stack = createStackNavigator();
 
 const App = () => {
+    const [loaded] = useFonts({
+        "Roboto-Black": require("./assets/fonts/Roboto-Black.ttf"),
+        "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+        "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    });
+
+    if (!loaded) {
+        return null;
+    }
+
     return (
         <NavigationContainer>
             <Stack.Navigator
