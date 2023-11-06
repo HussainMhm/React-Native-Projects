5d0d0b3912b66135911dd4496baa1323fd5143f9
hussain <hussainabdullatif@marun.edu.tr>
2023-11-07 00:56:17 +0300

    Update index.js

--- Cryptocurrency App UI/components/index.js
+++ Cryptocurrency App UI/components/index.js
@@ -1,3 +1,7 @@
 import PriceAlert from "./PriceAlert";
+import TransactionHistory from "./TransactionHistory";
+import HeaderBar from "./Headerbar";
+import CurrencyLabel from "./CurrencyLabel";
+import TextButton from "./TextButton";
 
-export { PriceAlert };
+export { PriceAlert, TransactionHistory, HeaderBar, CurrencyLabel, TextButton };
