8b9acd52ef76aa59a2fa4c3e77e94a9fee1f3905
hussain <hussainabdullatif@marun.edu.tr>
2023-11-07 20:10:47 +0300

    Added the header part

--- Travel App UI/screens/Place.js
+++ Travel App UI/screens/Place.js
@@ -1,15 +1,40 @@
-import React from 'react';
-import {
-    View,
-    Text
-} from 'react-native';
+import React, { useEffect, useState } from "react";
+import { View, Text, ImageBackground } from "react-native";
 
-const Place = () => {
+import { COLORS, SIZES, FONTS, icons } from "../constants";
+import { HeaderBar } from "../components";
+
+const Place = ({ route, navigation }) => {
+    const [selectedPlace, setSelectedPlace] = useState(null);
+
+    useEffect(() => {
+        let { selectedPlace } = route.params;
+        setSelectedPlace(selectedPlace); // there might be a conflict here
+    }, []);
+
+    function renderPlace() {
+        return (
+            <ImageBackground
+                source={selectedPlace?.image}
+                style={{ width: "100%", height: "100%" }}
+            >
+                <HeaderBar
+                    title=""
+                    leftOnPress={() => navigation.goBack()}
+                    right={false}
+                    containerStyle={{
+                        marginTop: SIZES.padding * 2,
+                    }}
+                />
+            </ImageBackground>
+        );
+    }
+
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Place</Text>
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+            {renderPlace()}
         </View>
-    )
-}
+    );
+};
 
 export default Place;
