71fa8c8c37e8fb2e2a246a4b2f6336b3e907d7f7
hussain <hussainabdullatif@marun.edu.tr>
2023-11-07 21:27:32 +0300

    Added the sliding up panel
    Implemented the Sliding Up Panel that includes a map view of the place selected and shows the hotels in the area.

--- Travel App UI/screens/Place.js
+++ Travel App UI/screens/Place.js
@@ -1,11 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { View, Text, ImageBackground, Image } from "react-native";
+import SlidingUpPanel from "rn-sliding-up-panel";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 
 import { COLORS, SIZES, FONTS, icons, images } from "../constants";
 import { HeaderBar, TextIconButton } from "../components";
+import MapStyle from "../styles/MapStyle";
 
 const Place = ({ route, navigation }) => {
     const [selectedPlace, setSelectedPlace] = useState(null);
+    const [selectedHotel, setSelectedHotel] = useState(null);
+
+    let _panel = useRef(null);
 
     useEffect(() => {
         let { selectedPlace } = route.params;
@@ -78,9 +84,79 @@ const Place = ({ route, navigation }) => {
         );
     }
 
+    function renderMap() {
+        return (
+            <SlidingUpPanel
+                ref={(c) => (_panel = c)}
+                draggableRange={{ top: SIZES.height + 120, bottom: 120 }}
+                showBackdrop={false}
+                snappingPoints={[SIZES.height + 120]}
+                height={SIZES.height + 120}
+                friction={0.7}
+            >
+                <View style={{ flex: 1, backgroundColor: "transparent" }}>
+                    {/* Panel Header */}
+                    <View
+                        style={{
+                            height: 120,
+                            backgroundColor: "transparent",
+                            alignItems: "center",
+                            justifyContent: "center",
+                        }}
+                    >
+                        <Image
+                            source={icons.up_arrow}
+                            style={{ width: 16, height: 16, tintColor: COLORS.lightGray }}
+                        />
+                        <Text style={{ color: COLORS.lightGray, ...FONTS.h4 }}>
+                            SWIPE FOR DETAILS
+                        </Text>
+                    </View>
+
+                    {/* Panel Detail */}
+                    <View
+                        style={{
+                            flex: 1,
+                            backgroundColor: COLORS.white,
+                            alignItems: "center",
+                            justifyContent: "center",
+                        }}
+                    >
+                        <MapView
+                            style={{ width: "100%", height: "100%" }}
+                            provider={PROVIDER_GOOGLE}
+                            region={selectedPlace?.mapInitialRegion}
+                            customMapStyle={MapStyle}
+                        >
+                            {selectedPlace?.hotels.map((hotel, index) => (
+                                <Marker
+                                    key={index}
+                                    coordinate={hotel.latlng}
+                                    identifier={hotel.id}
+                                    onPress={() => setSelectedHotel(hotel)}
+                                >
+                                    <Image
+                                        source={
+                                            selectedHotel?.id === hotel.id
+                                                ? icons.bed_on
+                                                : icons.bed_off
+                                        }
+                                        resizeMode="contain"
+                                        style={{ width: 50, height: 50 }}
+                                    />
+                                </Marker>
+                            ))}
+                        </MapView>
+                    </View>
+                </View>
+            </SlidingUpPanel>
+        );
+    }
+
     return (
         <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
             {renderPlace()}
+            {renderMap()}
         </View>
     );
 };
