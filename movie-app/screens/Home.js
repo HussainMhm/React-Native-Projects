603c18a5614261c0d229932befbee9451ea20f15
hussain <hussainabdullatif@marun.edu.tr>
2023-11-08 18:30:59 +0300

    Added animations for the pagination
    Dots change color and shape according to the current position.

--- movie-app/screens/Home.js
+++ movie-app/screens/Home.js
@@ -196,28 +196,48 @@ const Home = ({ navigation }) => {
     }
 
     function renderDots() {
+        const dotPosition = Animated.divide(newSeasonScrollX, SIZES.width);
+
         return (
             <View
                 style={{
                     marginTop: SIZES.padding,
-
                     flexDirection: "row",
                     alignItems: "center",
                     justifyContent: "center",
                 }}
             >
                 {dummyData.newSeason.map((item, index) => {
+                    const opacity = dotPosition.interpolate({
+                        inputRange: [index - 1, index, index + 1],
+                        outputRange: [0.3, 1, 0.3],
+                        extrapolate: "clamp",
+                    });
+
+                    const dotWidth = dotPosition.interpolate({
+                        inputRange: [index - 1, index, index + 1],
+                        outputRange: [6, 20, 6],
+                        extrapolate: "clamp",
+                    });
+
+                    const dotColor = dotPosition.interpolate({
+                        inputRange: [index - 1, index, index + 1],
+                        outputRange: [COLORS.lightGray, COLORS.primary, COLORS.lightGray],
+                        extrapolate: "clamp",
+                    });
+
                     return (
-                        <View
+                        <Animated.View
                             key={`dot-${index}`}
+                            opacity={opacity}
                             style={{
                                 borderRadius: SIZES.radius,
                                 marginHorizontal: 3,
-                                width: 6,
+                                width: dotWidth,
                                 height: 6,
-                                backgroundColor: COLORS.primary,
+                                backgroundColor: dotColor,
                             }}
-                        ></View>
+                        ></Animated.View>
                     );
                 })}
             </View>
