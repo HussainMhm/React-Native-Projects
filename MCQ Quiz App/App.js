be9618fd8660bc80ee903151b276115d140c84e6
hussain <hussainabdullatif@marun.edu.tr>
2023-11-09 18:27:12 +0300

    Initialize the quiz app project

---
+++ MCQ Quiz App/App.js
@@ -0,0 +1,10 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import { Quiz } from "./screens";
+
+export default function App() {
+    return <Quiz />;
+}
+
+const styles = StyleSheet.create({});
---
+++ MCQ Quiz App/screens/Quiz.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import {
+    View,
+    Text,
+    SafeAreaView,
+    StatusBar,
+    Image,
+    TouchableOpacity,
+    Modal,
+    Animated,
+} from "react-native";
+import { COLORS, SIZES } from "../constants";
+import data from "../data/QuizData";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+const Quiz = () => {
+    return (
+        <SafeAreaView>
+            <StatusBar barStyle="light-content" />
+            <View>
+                {/* Progress Bar */}
+
+                {/* Question */}
+
+                {/* Options */}
+
+                {/* Next Button */}
+
+                {/* Background Image */}
+            </View>
+        </SafeAreaView>
+    );
+};
+
+export default Quiz;
